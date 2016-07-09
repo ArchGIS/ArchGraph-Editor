91a03cd1f6aa82ec41ea5d8ffd5f17e379a0561c
quasilyte <quasilyte@gmail.com>
2016-07-09 20:06:48 +0300

    bind клавиш с комбинацией через ctrl

--- js/core/hotkey.js
+++ js/core/hotkey.js
@@ -4,17 +4,33 @@
   var bindings = {};
 
   function bindKeyUp(keyCode, callback) {
-    bindings[keyCode] = callback;
+    bindings[keyCode] = {
+      "withoutCtrl": true,
+      "fn": callback
+    };
+  }
+
+  function bindCtrlKeyUp(keyCode, callback) {
+    bindings[keyCode] = {
+      "withoutCtrl": false,
+      "fn": callback
+    };
   }
 
   $(window).keyup(function(event) {
-    var callback = bindings[event.which];
-    if (callback) {
-      callback();
+    var binding = bindings[event.which];
+    
+    if (binding) {
+      if (binding.withoutCtrl) {
+        binding.fn(event);
+      } else if (event.ctrlKey) {
+        binding.fn(event);
+      }
     }
   });
   
   App.hotkey = {
-    "bind": bindKeyUp
+    "bind": bindKeyUp,
+    "bindCtrl": bindCtrlKeyUp,
   };
 }());
