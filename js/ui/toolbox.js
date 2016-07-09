7163c9a5929c17bbe13d63a09f2d6b42d109994e
quasilyte <quasilyte@gmail.com>
2016-07-09 20:10:02 +0300

    обновил toolbox

--- js/ui/toolbox.js
+++ js/ui/toolbox.js
@@ -1,7 +1,62 @@
 "use strict";
 
-(function() {
-  var $ctlTab = $("#control-tab");
-  var $actionTab = $("#action-tab");
-  var $dataTab = $("#data-tab");
-}());
+$(function() {
+  var t = App.locale.translate;
+
+  var $selector = $("#tab-selector");
+  var $selected = null;
+  var tabs = [];
+  var tabNames = [];
+
+  /**
+   * @param {string[]} enabledTabs
+   */
+  function init(enabledTabs) {
+    tabNames = enabledTabs;
+
+    var options = [];
+
+    _.each(tabNames, (tabName, tabIndex) => {
+      var n = tabIndex + 1;
+      var text = t(`toolbox.${tabName}`);
+
+      options.push(`<option value="${tabIndex}">[${n}] ${text}</option>`);
+    });
+
+    $selector.html(options);
+
+    _.each(tabNames, (tabName) => {
+      tabs.push($("#" + tabName));
+    });
+
+    $selected = tabs[+$selector.find(":selected").val()];
+    $selected.show();
+
+    $selector.on("change", function() {
+      setTab(+$selector.find(":selected").val());
+    });
+  }
+
+  /**
+   * @param {number} tabIndex
+   */
+  function setTab(tabIndex) {
+    tabIndex = +tabIndex;
+    var $newTab = tabs[tabIndex];
+
+    if ($newTab) {
+      if ($newTab != $selected) {
+        $selected.hide();
+        $selected = $newTab;
+        $selected.show();
+
+        $selector.val(tabIndex);
+      }
+    }
+  }
+
+  App.toolbox = {
+    "setTab": setTab,
+    "init": init
+  };
+});
