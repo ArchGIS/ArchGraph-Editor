9f02acb7d70c64ab0122375aac94bda5a606eed5
quasilyte <quasilyte@gmail.com>
2016-07-14 15:59:17 +0300

    status label

--- js/inputs.js
+++ js/inputs.js
@@ -4,29 +4,31 @@ var INP_TEXT = 0;
 var INP_STRING = 1;
 
 (function() {
-  var inputId = 0;
+  var t = App.locale.translate;
 
-  function factory(type, name, value) {
-    switch (type) {
-    case INP_TEXT: return new TextInput(name, value);
-    case INP_STRING: return new StringInput(name, value);
-    default: throw `unknown input type: ${type}`;
-    }
-  }
-  
-  function StringInput(name, value) {
-    var $input = $(`<input value="${value}">`);
-    
-    this.val = () => $input.val();
-    this.$el = $(`<label>${name}${$input[0].outerHTML}</label>`);
-  }
-  
-  function TextInput(name, value) {
-    var $input = $(`<textarea>${value}</textarea>`);
-    
-    this.val = () => $input.val();
-    this.$el = $(`<label>${name}${$input[0].outerHTML}</label>`);
+  function NodeInputStatus(key) {
+    var $status = $(`#node-${key}-status`);
+
+    this.setNormalStatus = $status.hide;    
+
+    this.setErrorStatus = (hint) => {
+      $status.
+        show(). 
+        removeClass("status-ok").
+        addClass("status-error").
+        prop("title", hint);
+    };
+
+    this.setOkStatus = () => {
+      $status.
+        show().
+        removeClass("status-error").
+        addClass("status-ok").
+        prop("title", "");
+    };
   }
 
-  App.input = {"factory": factory};
-}());
+  App.inputs = {
+    "InputStatus": NodeInputStatus
+  };
+}())
